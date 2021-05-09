93a9fcc0461ad0baa8b8aca3ee4bba3caafe7996
Nick Hamm <nick.hamm@mac.com>
2021-05-09 18:45:52 -0400

    created shell script template

---
+++ vim/skeletons/shell-script.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#
+# INSERT DESCRIPTION OF SCRIPT HERE
+
+set -eou pipefail 
+# -e = if command fails immediately exit
+# -o pipefail = causes a pipeline (like curl) to fail if any command errors
+# -u = treats unset variables as an error and immediately exit
+
