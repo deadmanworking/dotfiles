8a835b401d95b6cc410c48d9a29a08d14840c448
Nick Hamm <nick.hamm@mac.com>
2021-05-09 14:44:54 -0400

    added template file for main.py

---
+++ vim/skeletons/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Docstrings go here
+"""
+
+__author__ = "Nick Hamm"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    """ Entry point """
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    """Executs when run from command line"""
+    main()
