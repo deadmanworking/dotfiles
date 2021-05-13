6c8d8f5a8d6176cc3cc21754f233b9cc698b1e5a
Nick Hamm <nick.hamm@mac.com>
2021-05-12 22:23:01 -0400

    notes to setup.sh

--- setup.sh
+++ setup.sh
@@ -9,4 +9,20 @@ set -eou pipefail
 # -o pipefail = causes a pipeline (like curl) to fail if any command errors
 # -u = treats unset variables as an error and immediately exit
 
+### How is this supposed to work?
+## Interactive 
+# - Checks the OS and/or processor (m1 or no?)
+# - Says hi (TODO: How can we find the user's first name everytime?)
+# - 2 choices: Select what is installed or just install it all
+# - if the latter, confirm with a list
+# - Script should create .zshrc (et al)
+## Non-interactive
+# 1. Clone Repo
+# 2. Create ~/.zshenv
+# 3. source ~/.zshenv
+# 4. Install Homebrew
+# 5. Do homebrew things
+
+
+
 
